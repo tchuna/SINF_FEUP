d5162d715165401e66794d6aa9f141ad05634fb0
rpedro10 <rpedro10@gmail.com>
2018-12-12 16:03:26 +0000

    deleted index.css

--- my_app/client/src/index.js
+++ my_app/client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
