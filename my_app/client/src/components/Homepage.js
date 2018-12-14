d447654521a0bb54854a58819b6c25f9f095f3c8
FabioTMW <fabiotmw@msn.com>
2018-12-14 16:42:25 +0000

    fixed classes

--- my_app/client/src/components/Homepage.js
+++ my_app/client/src/components/Homepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation.js';
 
-class Homepage extends Component {
+class HomepageSuggestions extends Component {
   constructor(props) {
     super(props);
  
@@ -19,55 +19,131 @@ class Homepage extends Component {
       // Fetch Products
   }
  
+  render () {
+    return ( 
+      <div className="mb-3">
+        <h4>Sugestões</h4>
+        <div className="row">
+
+          <div className="col-lg-2">
+            <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+class HomepageNew extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      data: null,
+    };
+ 
+  }
+ 
+  componentDidMount () {
+    this.getData();
+  }
+
+  getData () {
+      // Fetch Products
+  }
+ 
+  render () {
+    return ( 
+      <div className="mb-3">
+        <h4>Novidades</h4>
+        <div className="row">
+
+          <div className="col-lg-2">
+            <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+          <div className="col-lg-2">
+              <img className="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+class Homepage extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      data: null,
+    };
+ 
+  }
+ 
+  componentDidMount () {
+    this.getData();
+  }
+
+  getData () {
+      // Fetch User Related things
+  }
+ 
   render() {
     return (
       <div>
         <Navigation />
 
-        <div class="container main-container mt-5 mb-3">
+        <div className="container main-container mt-5 mb-3">
 
-          <div class="mb-3 row">
-            <div class="row">
+          <div className="mb-3 row">
+            <div className="row">
 
-              <div class="col-lg-6">
+              <div className="col-lg-6">
                 <h4>Fruta</h4>
-                <img class="img-fluid" src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11274/slide/alimentacao-cursos-cpt_frutas_2.jpg" alt="Chania"></img>
+                <img className="img-fluid" src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11274/slide/alimentacao-cursos-cpt_frutas_2.jpg" alt="Chania"></img>
               </div>
               
-              <div class="col-lg-6">
+              <div className="col-lg-6">
                   <h4>Legumes</h4>
-                  <img class="img-fluid" src="https://i.pinimg.com/originals/e9/eb/11/e9eb11aca59e512ccff2ef12a52c3669.jpg" alt="Chania"></img>
+                  <img className="img-fluid" src="https://i.pinimg.com/originals/e9/eb/11/e9eb11aca59e512ccff2ef12a52c3669.jpg" alt="Chania"></img>
               </div>
 
             </div>
           </div>
 
-          <div class="mb-3">
-            <h4>Sugestões</h4>
-            <div class="row">
+        <HomepageSuggestions />
 
-                <div class="col-lg-2">
-                  <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-                <div class="col-lg-2">
-                    <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-                <div class="col-lg-2">
-                    <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-                <div class="col-lg-2">
-                    <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-                <div class="col-lg-2">
-                    <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-                <div class="col-lg-2">
-                    <img class="img-fluid" src="https://uetitalia.it/wp-content/uploads/2018/09/blog-placeholder.png" alt="Chania"></img>
-                </div>
-
-              </div>
-            </div>
-
+        <HomepageNew />
+        
         </div>
 
       </div>
