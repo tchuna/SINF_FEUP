ebc21d6e9772f51eaa4f6186787e7ecd401ab724
FabioTMW <fabiotmw@msn.com>
2018-12-14 17:07:03 +0000

    homepage update

--- my_app/client/src/components/Homepage.js
+++ my_app/client/src/components/Homepage.js
@@ -99,6 +99,45 @@ class HomepageNew extends Component {
   }
 }
 
+class HomepageCategory extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      data: null,
+    };
+ 
+  }
+ 
+  componentDidMount () {
+    this.getData();
+  }
+
+  getData () {
+      // Fetch Products
+  }
+ 
+  render () {
+    return (
+      <div className="mb-3 row">
+        <div className="row">
+
+          <div className="col-lg-6">
+            <h4>Fruta</h4>
+            <img className="img-fluid" src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11274/slide/alimentacao-cursos-cpt_frutas_2.jpg" alt="Chania"></img>
+          </div>
+          
+          <div className="col-lg-6">
+              <h4>Legumes</h4>
+              <img className="img-fluid" src="https://i.pinimg.com/originals/e9/eb/11/e9eb11aca59e512ccff2ef12a52c3669.jpg" alt="Chania"></img>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
 class Homepage extends Component {
   constructor(props) {
     super(props);
@@ -124,21 +163,7 @@ class Homepage extends Component {
 
         <div className="container main-container mt-5 mb-3">
 
-          <div className="mb-3 row">
-            <div className="row">
-
-              <div className="col-lg-6">
-                <h4>Fruta</h4>
-                <img className="img-fluid" src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11274/slide/alimentacao-cursos-cpt_frutas_2.jpg" alt="Chania"></img>
-              </div>
-              
-              <div className="col-lg-6">
-                  <h4>Legumes</h4>
-                  <img className="img-fluid" src="https://i.pinimg.com/originals/e9/eb/11/e9eb11aca59e512ccff2ef12a52c3669.jpg" alt="Chania"></img>
-              </div>
-
-            </div>
-          </div>
+        <HomepageCategory />
 
         <HomepageSuggestions />
 
